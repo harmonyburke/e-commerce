83a7e4a7b5faa3c86a3e7afb3a04129f1a3657f4
Harmony Burke <theharmonyburke@gmail.com>
2023-12-15 17:58:39 -0600

    finihsed product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll().then((categoryData) =>{
-    res.jeson(categoryData);
+    res.json(categoryData);
   })
 });
 
